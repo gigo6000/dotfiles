1aa8c7fd1e2c2467fc930054bade6fa5558527fb
Carlos Mafla <gigo6000@hotmail.com>
2018-03-17 11:14:47 -0500

    Vim setup bash script

--- vim-setup.sh
+++ vim-setup.sh
@@ -4,10 +4,8 @@ mkdir ~/.vim/backups
 mkdir ~/.vim/swaps
 mkdir ~/.vim/undos
 
-git clone https://github.com/gigo6000/dotfiles.git
-
-cp dotfiles/.vimrc ~/  
-cp dotfiles/.vimrc.bundles ~/  
-cp -a dotfiles/.vim ~/
+cp .vimrc ~/  
+cp .vimrc.bundles ~/  
+cp -a .vim ~/
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
