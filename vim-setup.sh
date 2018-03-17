570099735a8a274099786e34defd187a6808de08
Carlos Mafla <gigo6000@hotmail.com>
2018-03-17 11:31:19 -0500

    Vim script updates

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash                                                                                                                                                                                                  
+# Copy vim files (or better symlink to make updates)
 cp .vimrc ~/  
 cp .vimrc.bundles ~/  
 cp -a .vim ~/
 
+# Create to override vim default locations
 mkdir ~/.vim/backups
 mkdir ~/.vim/swaps
 mkdir ~/.vim/undos
 
+# Get Vundle plugin
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Install Plugins
+vim +PluginInstall +qall
