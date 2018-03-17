afbbca1494ce965bca8724281ef170f31615ff0f
Carlos Mafla <gigo6000@hotmail.com>
2018-03-17 11:17:19 -0500

    Fix vim setup bash script

--- vim-setup.sh
+++ vim-setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash                                                                                                                                                                                                  
+cp .vimrc ~/  
+cp .vimrc.bundles ~/  
+cp -a .vim ~/
 
 mkdir ~/.vim/backups
 mkdir ~/.vim/swaps
 mkdir ~/.vim/undos
-
-cp .vimrc ~/  
-cp .vimrc.bundles ~/  
-cp -a .vim ~/
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
