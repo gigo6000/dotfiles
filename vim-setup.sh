5609a5b610bf8bba68c952c24a3cae63988315be
Carlos Mafla <gigo6000@hotmail.com>
2021-11-11 18:55:57 -0500

    Fix font setup

--- vim-setup.sh
+++ vim-setup.sh
@@ -15,8 +15,8 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # Install Plugins
 vim +PluginInstall +qall
 
-# Get Fonts (Asume ma)
-[ -d ~/Library/Fonts ] && cd ~/Library/Fonts && { curl -O 'https://github.com/ryanoasis/nerd-fonts/blob/b9e5b3a9a4a8237fc266ca39c67ff9e35675ebaf/patched-fonts/Meslo/S/Regular/complete/Meslo LG S Regular Nerd Font Complete.ttf'; cd -; }
-[ -d ~/Library/Fonts ] && cd ~/Library/Fonts && { curl -O 'https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Meslo/S/Italic/complete/Meslo LG S Italic Nerd Font Complete.ttf'; cd -; }
-[ -d ~/Library/Fonts ] && cd ~/Library/Fonts && { curl -O 'https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Meslo/S/Bold/complete/Meslo LG S Bold Nerd Font Complete.ttf'; cd -; }
-[ -d ~/Library/Fonts ] && cd ~/Library/Fonts && { curl -O 'https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Meslo/S/Bold-Italic/complete/Meslo LG S Bold Italic Nerd Font Complete.ttf'; cd -; }
+# Get Fonts (Asume MacOS supported)
+[ -d ~/Library/Fonts ] && cd ~/Library/Fonts && { curl -L -o 'Meslo LG S Regular Nerd Font Complete.ttf' 'https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Meslo/S/Regular/complete/Meslo%20LG%20S%20Regular%20Nerd%20Font%20Complete.ttf?raw=true'; cd -; }
+[ -d ~/Library/Fonts ] && cd ~/Library/Fonts && { curl -L -o 'Meslo LG S Bold Nerd Font Complete.ttf' 'https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Meslo/S/Bold/complete/Meslo%20LG%20S%20Bold%20Nerd%20Font%20Complete.ttf'; cd -; }
+[ -d ~/Library/Fonts ] && cd ~/Library/Fonts && { curl -L -o 'Meslo LG S Italic Nerd Font Complete.ttf' 'https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Meslo/S/Italic/complete/Meslo%20LG%20S%20Italic%20Nerd%20Font%20Complete.ttf'; cd -; }
+[ -d ~/Library/Fonts ] && cd ~/Library/Fonts && { curl -L -o 'Meslo LG S Bold Italic Nerd Font Complete.ttf' 'https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Meslo/S/Bold-Italic/complete/Meslo%20LG%20S%20Bold%20Italic%20Nerd%20Font%20Complete.ttf'; cd -; }
